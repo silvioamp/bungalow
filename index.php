3875fc351a7a8d38c6cff2c31a09bb2542a2298f
Silvio Augusto <silvioamp@gmail.com>
2020-11-13 08:21:33 -0300

    Meu primeiro commit

---
+++ index.php
@@ -0,0 +1,128 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <link rel="stylesheet" type="text/css" href="style.css">
+	<link rel="stylesheet" type="text/css" href="bootstrap/css/bootstrap.min">
+	<link rel="stylesheet" href="fontawesome/css/fontawesome.min.css">
+    <title>Bungalow</title>
+</head>
+<header>
+    <div class="container">
+        <div class="row">
+            <div class="col">
+                <img id=logo src="img/logo-bungalow">
+            </div>
+            <div class="col-8">
+                <nav>
+                    <ul>
+                        <li><a href="#">Quem somos</a></li>
+                        <li><a href="#">Cardápio</a></li>
+                        <li><a href="#">Fotos</a></li>
+                        <li><a href="#">Contato</a></li>
+                    </ul>
+                </nav>
+            </div>
+            <div class="col">Logo</div>
+			<div class="col">redes</div>
+		</div>
+        <div class="row">
+            <div class="col"></div>
+            <div class="col"><img src="img/logo-casa"></div>         
+            <div class="col"></div>
+        </div>
+        <div class="row">
+            <div class="col"></div>         
+            <div class="col">
+                <h1>Fresh, Vegan</h1>
+                <h1>& Delicious!</h1>
+            </div>
+            <div class="col"></div>
+            
+        </div>
+        <div class="row">
+			<div class="col"></div>
+			<div class="col"><p>PLANTED-BASE SUSHI</p></div>
+			<div class="col"></div>
+		</div>
+        <div class="row retangulo">
+			
+				<div class="col">Peça pelo What's</div>
+				<div class="col">Peça pelo Goomer</div>
+				<div class="col">Funcionamento</div>
+			
+		</div>
+    </div>
+
+</header>
+<div class="container">
+<section>
+	<article>
+		TEXTO
+	</article>
+</section>
+</div>
+<div id=barra-vegan>
+	<div class="container">
+		<div class="row">
+			<div class="col-3">
+				<figure><img src="img/icon-world" alt="our planet"></figure>
+			</div>
+			<div class="col-3">
+				<figure><img src="img/icon-heart" alt="compassion"></figure>
+			</div>
+			<div class="col-3">
+				<figure><img src="img/icon-health" alt="our health"></figure>
+			</div>
+			<div class="col-3">
+				<figure><img src="img/icon-animal" alt="the animals"></figure>
+			</div>
+		</div>
+	</div>
+</div>
+<div class="container">
+<div id=cardapio>
+	<nav>	
+		<ul>
+			<li><a href="#">Entradas</a></li>
+			<li><a href="#">Hot Roll</a></li>
+			<li><a href="#">Combinados</a></li>
+			<li><a href="#">Temakis</a></li>
+			<li><a href="#">Niguiris</a></li>
+			<li><a href="#">Uramakis e outros</a></li>
+			<li><a href="#">Joy's</a></li>
+			<li><a href="#">Bebidas</a></li>
+		</ul>
+	</nav>
+</div>
+<div id="galeria">
+	<picture>
+		galeria
+	</picture>
+</div>
+</div>
+
+<footer>
+	<div class="container">
+		<div class="row">
+			<div class="col">
+				<div class="row">
+					<div class="col">contato</div>
+					<div class="col">contato</div>
+					<div class="col">contato</div>
+				</div>
+			</div>
+			<div class="col">
+
+				<p>Seja bem vindo(a) ao nosso <strong>Bungalow</strong> e bom apetite</p>
+			</div>
+			<div class="col"></div>
+		</div>
+	</div>
+</footer>
+
+<script src="bootstrap/js/bootstrap.min"></script>
+
+</body>
+</html>
---
+++ wordpress/wp-content/themes/bungalow/footer.php
@@ -0,0 +1,4 @@
+<script src="/bootstrap/js/bootstrap.min"></script>
+
+</body>
+</html>
