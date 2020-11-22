72a293b5219a949914b541588ff212d8582ceadd
Silvio Augusto <silvioamp@gmail.com>
2020-11-22 13:25:15 -0300

    Atualizacao cardapio

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <!DOCTYPE html>
-<html>
+<html lang="pt-br">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
@@ -11,6 +11,8 @@
 	<link rel="stylesheet" type="text/css" href="style.css">
     <title>Bungalow</title>
 </head>
+
+<!-- Inicio Header !-->
 <header>
     <div class="container">
         <div class="row">
@@ -69,18 +71,20 @@
     </div>
 
 </header>
+<!-- Fim Header !-->
 
+<!-- Inicio Section !-->
 <div class="container">
 <section>
 	<article>
 		<div class="row">
 			<div class="col-md-6">
 			<h3>
-				Somos uma casa pequena e aconchegante, mas nossa vontade de crescer é tão grando quanto nosso propósito
+				<strong class="verde">Somos uma casa pequena e aconchegante, mas nossa vontade de crescer é tão grando quanto nosso propósito</strong>
 			</h3>
 			<p class="txt-principal">Com identidade um tanto rústica, com raízes na praia ou em lugares que têm conexão com a natureza, nasceu o <span class="cinza">Bungalow Plant-Based Sushi</span>. O primeiro capítulo de nossa história começa na cidade de Santos, mas em breve nos veremos pelo Brasil!</p>
 		
-			<a href="#">Saiba mais sobre a gente</a>
+			<a  class="txt-verde" href="#"><strong class="verde">Saiba mais sobre a gente</strong></a>
 			<hr>
 			</div>
 			<div class="col-md-6">
@@ -102,7 +106,7 @@
 </p>
 			<p class="txt-secundario">Da conexão que temos com a natureza fazemos sushi vegano à base de plantas, tão gostoso e saboroso quanto o sushi tradicional feito com peixe. Aqui você irá se sentir em casa ao provar alimentos frescos, saborosos, naturais e orgânicos, que respeitam todos os seres vivos, principalmente os animais.</p>
 			<p class="txt-secundario">Prometemos levar a qualidade do melhor sushi plant-based até o conforto do seu Bungalow. Valorizamos a sustentabilidade, alimentamos a alma e proporcionamos a você a experiência de uma dark-kitchen, onde o atendimento é feito totalmente pela internet, sem ser presencial!</p>
-			<p class="txt-verde">Seja bem-vindo(a) ao nosso Bungalow e bom apetite!</p>	
+			<p class="txt-verde"><strong class="verde">Seja bem-vindo(a) ao nosso Bungalow e bom apetite!</strong></p>	
 		</div>
 			
 		</div>
@@ -110,6 +114,9 @@
 	
 </section>
 </div>
+<!-- Fim Section !-->
+
+<!-- Inicio Barra Vegan For !-->
 <div class="card">
 	<div class="container"> 
 		<div class="row">
@@ -135,6 +142,9 @@
 		</div>
 	</div>
 </div>
+<!-- Fim Barra Vegan For !-->
+
+<!-- Inicio Cardapio !-->
 <div class="container">
 	<div id=cardapio>
 		<nav>	
@@ -149,14 +159,271 @@
 				<li class="nav-item"><a class="nav-link active menu-secundario" href="#">Bebidas</a></li>
 			</ul>
 		</nav>
+		<div class="row">
+			<div class="col-sm-4 "><h4 class="txt-verde espaco-cardapio">Entradas Quentes</h4></div>
+			<div class="col-sm-2"></div>
+			<div class="col-sm-4"></div>
+			<div class="col-sm-2"></div>
+		</div>
+		<div class="row">
+			<div class="col-sm-4"><h5>Missshiro com Tofu - 200g</h5><p>Missoshiro com tofu, moiashi e cebolinha.</p></div>
+			<div class="col-sm-2"><p>R$ 18,00</p></div>
+			<div class="col-sm-4"><h5>Guioza na Capa de Repolho - 6 Unidades</h5><p>Guioza na chapa de repolho, cenoura, gengibre e cebolinha.</p></div>
+			<div class="col-sm-2"><p>R$29,00</p></div>
+		</div>
+		<div class="row">
+			<div class="col-sm-4"><h5>Shitake na Manteiga - 160g</h5><p>Shitake na manteiga vegana (Básico) e cebolinha.</p></div>
+			<div class="col-sm-2"><p>R$ 42,00</p></div>
+			<div class="col-sm-4"><h5>Guioza na Chapa de Shimeji - 6 Unidades</h5><p>Guioza na chapa de shimeji, alho poró, nirá e cebolinha.</p></div>
+			<div class="col-sm-2"><p>R$29,00</p></div>
+		</div>
+		<div class="row">
+			<div class="col-sm-4"><h5>Shimeji Preto na Manteiga - 160g</h5><p>Shitake na manteiga vegana (Básico) e cebolinha.</p></div>
+			<div class="col-sm-2"><p>R$ 42,00</p></div>
+			<div class="col-sm-4"><h5>Edamame na Fava - 200g</h5><p>Shimeji na manteiga vegana (Básico) e cebolinha.</p></div>
+			<div class="col-sm-2"><p>R$32,00</p></div>
+		</div>
+		<div class="row">
+			<div class="col-sm-4"><h5>Harumaki de Shitake e Cebola - 4 Unidades</h5><p>Harumaki de shitake, cebola caramelizada e aspargos.</p></div>
+			<div class="col-sm-2"><p>R$ 41,00</p></div>
+			<div class="col-sm-4"><h5>Robata de Cogumelos - 200g</h5><p>Harumaki de shitake, cebola caramelizada e aspargos.</p></div>
+			<div class="col-sm-2"><p>R$46,00</p></div>
+		</div>
+		<div class="row">
+			<div class="col-sm-4"><h5>Harumaki de Shitake e Shimeji - 4 Unidades</h5><p>Harumaki de shitake, shimeji preto e aspargos.</p></div>
+			<div class="col-sm-2"><p>R$ 43,00</p></div>
+			<div class="col-sm-4"><h5>Kakiague de Legumes - 120g</h5><p>Kakiague de cenoura, vagem holandesa, abobrinha, brócolis e cebola.</p></div>
+			<div class="col-sm-2"><p>R$34,00</p></div>
+		</div>
+		<div class="row">
+			<div class="col-sm-4 "><h4 class="txt-verde espaco-cardapio">Entradas Frias</h4></div>
+			<div class="col-sm-2"></div>
+			<div class="col-sm-4"></div>
+			<div class="col-sm-2"></div>
+		</div>
+		<div class="row">
+			<div class="col-sm-4"><h5>Sunomono - aprox. 160g</h5><p>Pepino fatiado na conserva, cenoura e gergelim.</p></div>
+			<div class="col-sm-2"><p>R$ 13,00</p></div>
+			<div class="col-sm-4"><h5>Baterá de Cogumelos - 8 unidades</h5><p>Baterá de cogumelos, maionese de wassabi e crispy de cebola.</p></div>
+			<div class="col-sm-2"><p>R$39,00</p></div>
+		</div>
+		<div class="row">
+			<div class="col-sm-4"><h5>Raiz de Bardana - aprox. 160g </h5><p>Raiz de bardana em conserva finalizada com óleo de gergelim.</p></div>
+			<div class="col-sm-2"><p>R$ 25,00</p></div>
+			<div class="col-sm-4"><h5>Kimchi - aprox. 160g</h5><p>Kimchi em conserva de acelga e pimenta koreana.</p></div>
+			<div class="col-sm-2"><p>R$13,00</p></div>
+		</div>
+		<div class="row">
+			<div class="col-sm-4"><h5>Rolinho de Papel de Arroz - 2 unidades</h5><p>Rolinho feito com cenoura, pepino, aspargos, vagem holandesa, tofu defumado e gergelim. Acompanha molho à base de shoyo especial, nabo ralado, gengibre ralado e sakê mirim.
 
+</p></div>
+			<div class="col-sm-2"><p>R$ 39,00</p></div>
+			<div class="col-sm-4"><h5>Tempurá de Tofu - 8 unidades</h5><p>Tempurá de tofu defumado, guacamole e broto de coentro.</p></div>
+			<div class="col-sm-2"><p>R$39,00</p></div>
+		</div>
+		<div class="row">
+			<div class="col-sm-4 "><h4 class="txt-verde espaco-cardapio">Hot Roll</h4></div>
+			<div class="col-sm-2"></div>
+			<div class="col-sm-4"></div>
+			<div class="col-sm-2"></div>
+		</div>
+		<div class="row">
+			<div class="col-sm-4"><h5>Hot Roll de Aspargos e Shimeji</h5><p>Hot roll com maionese de pimenta defumada, crispy de couve na manteiga, cebolinha e lasca de dedo de moça.
+</p></div>
+			<div class="col-sm-2"><p>R$ 29,00</p></div>
+			<div class="col-sm-4"><h5>Hot Roll Bungalow</h5><p>Hot roll de vagem holandesa, shitake e queijo chévre com crispy de alho porró e cebolinha.</p></div>
+			<div class="col-sm-2"><p>R$39,00</p></div>
+		</div>
+
+		<div class="row">
+			<div class="col-sm-4 "><h4 class="txt-verde espaco-cardapio">Combinados</h4></div>
+			<div class="col-sm-2"></div>
+			<div class="col-sm-4"></div>
+			<div class="col-sm-2"></div>
+		</div>
+		<div class="row">
+			<div class="col-sm-10"><h5>Combinado Sugoi - 23 peças</h5>
+			<p>1 Appa maki, 1 asparagusu maki, 1 uramaki califórnia, 1 niguiri de pimentão vermelho, 1 niguiri de abobrinha, 1 niguiri de aspargos confit, 1 niguiri de shitake, 1 niguiri de avocado, 1 niguiri de nirá, 1 uramaki tempurá, 1 niguiri de pimentão amarelo, 1 niguiri de cogumelo eringui, 1 futomaki de berinjela defumada, 1 baterá de cogumelos, 1 uramaki caponata, 1 acelgamaki de kiimchi, 1 joy a italiana, 1 joy de alga e wakame, 1 joy de alga e shimeji, 1 joy de pepino e guacamole, 1 joy de abobrinha.</p></div>
+			<div class="col-sm-2"><p>R$ 72,00</p></div>
+		</div>
+		<div class="row">
+			<div class="col-sm-10"><h5>Combinado Heikin Saizu - 16 peças</h5>
+			<p>1 Niguiri de aspargos confit, 1 niguiri de shitake, 1 niguiri de avocado, 1 niguiri de nirá, 1 uramaki tempurá, 1 niguiri de pimentão amarelo, 1 acelgamaki e kiimchi, 1 uramaki caponata, 1 baterá de cogumelos, 1 futomaki de berinjela defumada, 1 niguiri de cogumelo eringui, 1 joy de abobrinha, 1 joy de pepino e guacamole, 1 joy de alga e shimeji, 1 joy de alga e wakame, 1 joy a italiana.</p></div>
+			<div class="col-sm-2"><p>R$ 62,00</p></div>
+		</div>
+		<div class="row">
+			<div class="col-sm-10"><h5>Combinado Sukoshi - 12 Peças </h5>
+			<p>1 Baterá de cogumelos, 1 futomaki de couve manteiga e berinjela defumada, 1 niguiri de shitake, 1 joy de abobrinha e pimetões confit, 1 jyo de pepino e guaca mole, 1 uramaki tempurá, 1 uramaki caponata, 1 joy de alga e shimeji, 1 niguiri de nirá, 1 iguiri pimentão vermelho, 1 niguiri de abobrinha, 1 niguiri de aspargos.</p></div>
+			<div class="col-sm-2"><p>R$ 52,00</p></div>
+		</div> 
+		<div class="row">
+			<div class="col-sm-4 "><h4 class="txt-verde espaco-cardapio">Temakis</h4></div>
+			<div class="col-sm-2"></div>
+			<div class="col-sm-4"></div>
+			<div class="col-sm-2"></div>
+		</div>
+		<div class="row">
+			<div class="col-sm-4"><h5>Temaki de Shimeji Preto na Manteiga</h5><p>Temaki feito com shimeji na manteiga vegana e cabolinha.</p></div>
+			<div class="col-sm-2"><p>R$ 29,00</p></div>
+			<div class="col-sm-4"><h5>Temaki de Shitake na Manteiga</h5><p>Shitaje na manteiga vegana e nirá grelhado.</p></div>
+			<div class="col-sm-2"><p>R$29,00</p></div>
+		</div>
+		<div class="row">
+			<div class="col-sm-4"><h5>Temaki de Mix de Cogumelos e Maionese de Wassabi</h5><p>Temaki feito com cogumelos e maionese de wassabi.</p></div>
+			<div class="col-sm-2"><p>R$ 29,00</p></div>
+			<div class="col-sm-4"><h5>Temaki Caponata</h5><p>Temaki feito com abobrinha, berinjela e pimentão vermelho.</p></div>
+			<div class="col-sm-2"><p>R$22,00</p></div>
+		</div>
+		<div class="row">
+			<div class="col-sm-4"><h5>Temaki Califórnia </h5><p>Temaki feito com avocado, manga, broto de coentro, pepino e gergelim.</p></div>
+			<div class="col-sm-2"><p>R$ 22,00</p></div>
+			<div class="col-sm-4"><h5>Temaki de Kakiaguê Fry</h5><p>Temaki de cogumelos fry, batido com maionese de wassabi, cebolinha e gergelim.</p></div>
+			<div class="col-sm-2"><p>R$29,00</p></div>
+		</div>
+		<div class="row">
+			<div class="col-sm-4 "><h4 class="txt-verde espaco-cardapio">Niguiris - 2 Unidades</h4></div>
+			<div class="col-sm-2"></div>
+			<div class="col-sm-4"></div>
+			<div class="col-sm-2"></div>
+		</div>
+		<div class="row">
+			<div class="col-sm-4"><h5>Niguiri de Pimentão Vermelho Confitado</h5><p>Niguiri feito com pimentão vermelho confitado , raiz forte, broto de manjericão e crispy de cebola.</p></div>
+			<div class="col-sm-2"><p>R$ 14,00</p></div>
+			<div class="col-sm-4"><h5>Niguiri de Nirá Grelhado</h5><p>Niguiri feito com nirá grelhado, crispy de cebola e maionese de alho assado.</p></div>
+			<div class="col-sm-2"><p>R$14,00</p></div>
+		</div>
+		<div class="row">
+			<div class="col-sm-4"><h5>Niguiri de Pimentão Amarelo Confitado</h5><p>Niguiri feito com pimentão amarelo confitado, missô, broto de manjericão e crispy de alho poró.</p></div>
+			<div class="col-sm-2"><p>R$ 14,00</p></div>
+			<div class="col-sm-4"><h5>Niguiri de Avocado</h5><p>Niguiri feito com avocado, limão siciliano e broto de coentro.</p></div>
+			<div class="col-sm-2"><p>R$17,00</p></div>
+		</div>
+		<div class="row">
+			<div class="col-sm-4"><h5>Niguiri de Aspargos Confitados</h5><p>Niguiri feito com aspargos confitados, maionese de alho assado e flor de sal.</p></div>
+			<div class="col-sm-2"><p>R$ 19,00</p></div>
+			<div class="col-sm-4"><h5>Niguiri de Abobrinha Confitada</h5><p>Niguiri feito com abobrinha confit, maionese de pimenta defumada e pimenta togarashi.</p></div>
+			<div class="col-sm-2"><p>R$14,00</p></div>
+		</div>
+		<div class="row">
+			<div class="col-sm-4"><h5>Niguiri de Shitake e Aspargos Confitados</h5><p>Niguiri feito com shitake na manteiga vegana, aspargos confitados, cebolinha e pimenta layu.</p></div>
+			<div class="col-sm-2"><p>R$ 22,00</p></div>
+			<div class="col-sm-4"><h5>Niguiri de Cogumelo Eringui e Nirá</h5><p>Niguiri feito com cogumelo eringui na manteiga vegana, azeite trufado, nirá, maionese de pimenta defumada e cebolinha.</p></div>
+			<div class="col-sm-2"><p>R$18,00</p></div>
+		</div>
+		<div class="row">
+			<div class="col-sm-4 "><h4 class="txt-verde espaco-cardapio">Hossomakis, Futomakis e Uramakis</h4></div>
+			<div class="col-sm-2"></div>
+			<div class="col-sm-4"></div>
+			<div class="col-sm-2"></div>
+		</div>
+		<div class="row">
+			<div class="col-sm-4"><h5>Kappa Maki - 8 unidades  </h5><p>Kappa maki - pepino, alga e arroz.</p></div>
+			<div class="col-sm-2"><p>R$ 21,00</p></div>
+			<div class="col-sm-4"><h5>Acelgamaki de Kiimchi - 8 unidades</h5><p>Acelga em conserva, cenoura, cebolinha e pimenta coreana.</p></div>
+			<div class="col-sm-2"><p>R$29,00</p></div>
+		</div>
+		<div class="row">
+			<div class="col-sm-4"><h5>Uramaki Califórnia - 8 unidades</h5><p>Uramaki, manga, pepino, guacamole e broto de coentro.</p></div>
+			<div class="col-sm-2"><p>R$ 32,00</p></div>
+			<div class="col-sm-4"><h5>Asuparagasu Maki - 8 unidades</h5><p>Aspargos, alga e arroz.</p></div>
+			<div class="col-sm-2"><p>R$30,00</p></div>
+		</div>
+		<div class="row">
+			<div class="col-sm-4"><h5>Uramaki Tempurá - 8 unidades</h5><p>Uramaki, cenoura, vagem holandesa, aspargos, avocado, maionese de wassabi e crispy de cebola.</p></div>
+			<div class="col-sm-2"><p>R$ 39,00</p></div>
+			<div class="col-sm-4"><h5>Futomaki Couve Manteiga e Berinjela Defumada</h5><p>Futomaki feito com crispy de couve manteiga.</p></div>
+			<div class="col-sm-2"><p>R$ 29,00</p></div>
+		</div>
+		<div class="row">
+			<div class="col-sm-4"><h5>Uramaki Caponata - 8 unidades</h5><p>Antepasto de berinjela, abobrinha, pimentões e alho finalizado com crispy de cebola e maionese de alho assado.</p></div>
+			<div class="col-sm-2"><p>R$ 39,00</p></div>
+		</div>
+		<div class="row">
+			<div class="col-sm-4 "><h4 class="txt-verde espaco-cardapio">Joy's - 2 Unidades</h4></div>
+			<div class="col-sm-2"></div>
+			<div class="col-sm-4"></div>
+			<div class="col-sm-2"></div>
+		</div>
+		<div class="row">
+			<div class="col-sm-4"><h5>Joy de Cenoura, Edamame e Flôr de Sal </h5><p>Joy feito com edamame em grãos no vapor.</p></div>
+			<div class="col-sm-2"><p>R$ 14,00</p></div>
+			<div class="col-sm-4"><h5>Joy de Pepino e Guacamole</h5><p>Joy feito com fita de pepino, guacamole e broto de coentro.</p></div>
+			<div class="col-sm-2"><p>R$ 14,00</p></div>
+		</div>
+		<div class="row">
+			<div class="col-sm-4"><h5>Joy de Nabo e Romã</h5><p>Joy feito com fita de nabo, romã e broto de manjericão.</p></div>
+			<div class="col-sm-2"><p>R$ 13,00</p></div>
+			<div class="col-sm-4"><h5>Joy de Alga e Shimeji</h5><p>Joy feito com fita de alga marinha, shimeji na manteiga, nirá grelhado e maionese de wassabi.</p></div>
+			<div class="col-sm-2"><p>R$ 14,00</p></div>
+		</div>
+		<div class="row">
+			<div class="col-sm-4"><h5>Joy de Abobrinha e Pimentões</h5><p>Joy feito com fita de abobrinha, mix de pimentões confitados e brotos de manjericão
+</p></div>
+			<div class="col-sm-2"><p>R$ 13,00</p></div>
+			<div class="col-sm-4"><h5>Joy a Italiana</h5><p>Joy feito com fita de abobrinha maçaricada com azeite e flor de sal, tomatinhos cereja confit, tofu, pesto de manjericão, queijo chévre vegano e broto de manjericão.</p></div>
+			<div class="col-sm-2"><p>R$21,00</p></div>
+		</div>
+		<div class="row">
+			<div class="col-sm-4"><h5>Joy de Alga e Wakamê</h5><p>Joy feito com fita de alga marinha, wakamê e cebolinha.</p></div>
+			<div class="col-sm-2"><p>R$ 16,00</p></div>
+			
+		</div>
+		<div class="row">
+			<div class="col-sm-4 "><h4 class="txt-verde espaco-cardapio">Bebidas</h4></div>
+			<div class="col-sm-2"></div>
+			<div class="col-sm-4"></div>
+			<div class="col-sm-2"></div>
+		</div>
+		<div class="row">
+			<div class="col-sm-4"><h5>Água Sem Gás – 310ml</h5></div>
+			<div class="col-sm-2"><p>R$ 29,00</p></div>
+			<div class="col-sm-4"><h5>Guaraná Antártica Zero – 350ml</h5></div>
+			<div class="col-sm-2"><p>R$ 29,00</p></div>
+		</div>
+		<div class="row">
+			<div class="col-sm-4"><h5>Água Com Gás – 310ml</h5></div>
+			<div class="col-sm-2"><p>R$ 29,00</p></div>
+			<div class="col-sm-4"><h5>Chá Branco com Lichia Feel Good – 330ml</h5></div>
+			<div class="col-sm-2"><p>R$ 29,00</p></div>
+		</div>
+		<div class="row">
+			<div class="col-sm-4"><h5>Água de Coco Natural – Eden Coco</h5></div>
+			<div class="col-sm-2"><p>R$ 29,00</p></div>
+			<div class="col-sm-4"><h5>Chá Verde Feel Good – 330ml</h5></div>
+			<div class="col-sm-2"><p>R$ 29,00</p></div>
+		</div>
+		<div class="row">
+			<div class="col-sm-4"><h5>Coca-Cola Original – 350ml</h5></div>
+			<div class="col-sm-2"><p>R$ 29,00</p></div>
+			<div class="col-sm-4"><h5>Stella Artois – 330ml</h5></div>
+			<div class="col-sm-2"><p>R$ 29,00</p></div>
+		</div>
+		<div class="row">
+			<div class="col-sm-4"><h5>Coca-Cola Zero – 350ml</h5></div>
+			<div class="col-sm-2"><p>R$ 29,00</p></div>
+			<div class="col-sm-4"><h5>Heineken – 330ml</h5></div>
+			<div class="col-sm-2"><p>R$ 29,00</p></div>
+		</div>
+		<div class="row">
+			<div class="col-sm-4"><h5>Guaraná Antártica – 350ml</h5></div>
+			<div class="col-sm-2"><p>R$ 29,00</p></div>
+			<div class="col-sm-4"><h5>Saquê Azuma Nama – 750ml</h5></div>
+			<div class="col-sm-2"><p>R$ 29,00</p></div>
+		</div>
+		</div>
 	</div>	
 </div>
+<!-- Fim cardapio !-->
+
+<!-- Iniccio Galeria !-->
 <div id="galeria">
 	<picture>
 		galeria
 	</picture>
 </div>
+<!-- Fim Galeria !-->
+
+<!-- Inicio Footer !-->
 <footer>
 	<div class="container">
 		<div class="row">
@@ -184,8 +451,9 @@
 		</div>
 	</div>
 </footer>
+<!-- Fim Footer !-->
 
-
+<!-- script js para bootstrap !-->
 <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ho+j7jyWK8fNQe+A12Hb8AhRq26LrZ/JpcUGGOn+Y7RsweNrtN/tE3MoK7ZeZDyx" crossorigin="anonymous"></script>
 </body>
